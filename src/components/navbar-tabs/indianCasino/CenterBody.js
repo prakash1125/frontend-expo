51ca288e2d0edbe93636c0b444553e1b66cf9b79
Pranav <pranav@avalontribe.com>
2023-05-11 19:37:04 +0530

    Indian Casino completed

--- src/components/navbar-tabs/indianCasino/CenterBody.js
+++ src/components/navbar-tabs/indianCasino/CenterBody.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import "./CenterBody.scss"
 import { IndianCasino } from "../../../utils/constant.js";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+SwiperCore.use([Navigation]);
+
 
 const CenterBody = ({ tabname }) => {
+    const params = {
+        autoplay: {
+            delay: 2000, // 5 seconds
+        },
+        spaceBetween: 10,
+        slidesPerView: 3,
+        loop: true,
+        navigation: {
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            el: '.swiper-pagination',
+        },
+    };
+    const handlePrevClick = () => {
+        console.log('Previous button clicked');
+      };
+
     return (
-        <div className='casino-container'>
-            
+        <div className='casino-container' >
+            <div className="swiper-container">
+                <Swiper {...params}>
+                    {IndianCasino.map((element, index) => (
+                        <SwiperSlide key={index}>
+                            <img src={element.img} alt={`Image ${index}`} />
+                        </SwiperSlide>
+                    ))}
+                    <div className="swiper-button-prev" onClick={handlePrevClick}></div>
+                    <div className="swiper-button-next"></div>
+                </Swiper>
+            </div>
             <h3 className='casino-tab'>{tabname}</h3>
 
             <div className='image-container'>
@@ -26,8 +58,9 @@ const CenterBody = ({ tabname }) => {
                 }
             </div>
 
-        </div>
+        </div >
     )
 }
+
 
 export default CenterBody
