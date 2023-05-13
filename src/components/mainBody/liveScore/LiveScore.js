45f2eac69c1ed5d381e1f8253f9fe83dd583a6ec
Pranav <pranav@avalontribe.com>
2023-05-13 21:39:42 +0530

    Improved Tabs in home page and sports page

--- src/components/mainBody/liveScore/LiveScore.js
+++ src/components/mainBody/liveScore/LiveScore.js
@@ -7,9 +7,14 @@ import LiveData from './LiveData'
 import BodyCarousel from '../bodyCarousel/BodyCarousel'
 
 const LiveScore = () => {
-    const [expandedTable, setExpandedTable] = useState(null);
+    const [expandedTables, setExpandedTables] = useState([]);
+
     const handleClick = (index) => {
-        setExpandedTable(expandedTable === index ? null : index);
+        if (expandedTables.includes(index)) {
+            setExpandedTables(expandedTables.filter((item) => item !== index));
+        } else {
+            setExpandedTables([...expandedTables, index]);
+        }
     };
 
     return (
@@ -30,11 +35,11 @@ const LiveScore = () => {
             {liveScores.map((data, index) => (
                 <div key={index} className='liveScore-table'>
                     <button onClick={() => handleClick(index)} type="button" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                        {/* <element.icon /> */}
+                        
                         <span className="flex-1 ml-3 text-left tab-name whitespace-nowrap">{data.tabName}</span>
-                        <svg className={`w-6 h-6 ${expandedTable === index ? 'rotate-animation' : 'remove-rotation-animation'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                        <svg className={`w-6 h-6 ${expandedTables.includes(index) ? 'rotate-animation' : 'remove-rotation-animation'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </button>
-                    <div className={`transition-transform translate-x-0 ${expandedTable === index ? "hidden" : "block"}`}>
+                    <div className={`transition-transform ${expandedTables.includes(index) ? 'translate-x-0' : 'translate-x-full'} ${expandedTables.includes(index) ? 'block' : 'hidden'}`}>
                         {data.placeScores.map((element, index) => {
                             return (
                                 <LiveData key={index} element={element} />
