5f0c26e5040d0bc297bdbb88ddfb154060da81dc
HJ-AT <hardik.joshi@avalontribe.com>
2023-05-26 19:10:37 +0400

    redux

--- src/redux/actions/types/index.js
+++ src/redux/actions/types/index.js
@@ -1,1 +1,8 @@
 //All types for Actions need to be created here and export, this will be used as action types
+
+// =====================Get Sports Name===========================
+
+
+
+
+// =====================Get Event ================================
