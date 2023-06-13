178fc14272d5c938123e4d51dbe2f64a7dc1ed2e
HJ-AT <hardik.joshi@avalontribe.com>
2023-06-13 16:13:17 +0400

    Minor changes in sidabar

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 import { RiArrowUpSLine } from "react-icons/ri";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
@@ -119,20 +120,19 @@ export const SideNavbar = () => {
 
   return (
     <>
-      <div className="w-full px-4 pt-2 z-40">
+      <div className="w-full px-4 pl-3 pt-2 z-40">
         <div className="flex flex-col justify-start mb-2">
           <button className="flex justify-start text-skin-primary  bg-skin-cardhead p-3 rounded-md w-full text-xs font-semibold relative">
             SPORTS
           </button>
         </div>
       </div>
-      <ul className="w-full px-4">
+      <ul className="w-full px-4 pl-3">
         {data?.map((item, index) => (
           <li
             key={index}
-            className={`cursor-pointer flex flex-col justify-between  mb-2 py-3   w-full  hover:bg-skin-cardhead hover:duration-200 rounded-md ${
-              activeIndex === index ? "bg-skin-cardhead" : ""
-            }`}
+            className={`cursor-pointer flex flex-col justify-between  mb-2 py-3   w-full  hover:bg-skin-cardhead hover:duration-200 rounded-md ${activeIndex === index ? "bg-skin-cardhead" : ""
+              }`}
           >
             <div
               onClick={() => toggleAccordion(index)}
@@ -153,11 +153,11 @@ export const SideNavbar = () => {
                   <span className="flex justify-center bg-skin-imgbg rounded-sm w-5 h-4 rounded-xs font-semibold text-xs">
                     {item?.leagues ? item?.leagues?.length : 0}
                   </span>
-                  <h5 className="flex justify-center text-skin-secondary  cursor-pointer">
+                  <h5 className=" text-sm justify-center text-skin-secondary  cursor-pointer">
                     {activeIndex === index ? (
-                      <RiArrowUpSLine className="font-semibold" />
+                      <IoIosArrowUp />
                     ) : (
-                      <IoIosArrowDown className="font-semibold" />
+                      <IoIosArrowDown />
                     )}
                   </h5>
                 </div>
@@ -169,7 +169,7 @@ export const SideNavbar = () => {
                   <div
                     key={index}
                     onClick={() => toggleDropdown(index)}
-                    className="justify-between pl-3 pr-4 rounded-md py-3 hover:bg-skin-hovercolorsecondary flex w-full"
+                    className="justify-between pl-3 pr-[15px] rounded-md py-3 hover:bg-skin-hovercolorsecondary flex w-full"
                   >
                     <div>
                       <div className="text-skin-secondary block text-[12px] font-semibold">
@@ -191,7 +191,7 @@ export const SideNavbar = () => {
                               }}
                             >
                               <div className="justify-between pl-2 pr-4 rounded-md py-3 hover:bg-skin-cardhead flex w-full">
-                                <div className="text-skin-secondary block text-xs w-full font-semibold">
+                                <div className="text-skin-primary block text-xs w-full font-semibold">
                                   {event?.name}
                                 </div>
                               </div>
@@ -200,7 +200,13 @@ export const SideNavbar = () => {
                         </div>
                       )}
                     </div>
-                    <IoIosArrowDown className="font-semibold text-white text-sm" />
+                    <h5 className="w-1 text-sm text-skin-secondary  cursor-pointer">
+                      {dropdownIndex === index ? (
+                        <IoIosArrowUp className="" />
+                      ) : (
+                        <IoIosArrowDown className="" />
+                      )}
+                    </h5>
                   </div>
                 ))}
               </div>
